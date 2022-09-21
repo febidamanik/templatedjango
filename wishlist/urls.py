217ff5e88f4c2eac3dd441a57cb361c58a70b206
febidamanik <febidamanik@gmail.com>
2022-09-22 00:33:46 +0700

    lab3-pbp

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from wishlist.views import show_wishlist
 from wishlist.views import show_wishlist, show_xml, show_json, show_xml_by_id, show_json_by_id
-app_name = 'wishlist'
+from wishlist.views import register
+from wishlist.views import login_user
+from wishlist.views import logout_user
+
+app_name = 'wishlist' 
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
@@ -9,4 +13,7 @@ urlpatterns = [
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/', show_json, name='show_json'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
