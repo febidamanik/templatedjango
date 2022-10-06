f852251492a97cd1053fd3211224ca454b2ec8c9
febidamanik <febidamanik@gmail.com>
2022-10-06 14:22:36 +0700

    add lab5-pbp

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
-from wishlist.views import show_wishlist
+from wishlist.views import show_wishlist, show_wishlist_ajax
 from wishlist.views import show_wishlist, show_xml, show_json, show_xml_by_id, show_json_by_id
 from wishlist.views import register
 from wishlist.views import login_user
 from wishlist.views import logout_user
+from wishlist.views import submit_ajax
 
 app_name = 'wishlist' 
 
 urlpatterns = [
     path('', show_wishlist, name='show_wishlist'),
+    path('ajax/', show_wishlist_ajax, name='show_wishlist_ajax'),
+    path('ajax/submit', submit_ajax, name='submit_ajax'),
     path('xml/', show_xml, name='show_xml'),
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/', show_json, name='show_json'),
--- wishlist/views.py
+++ wishlist/views.py
@@ -18,10 +18,31 @@ def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
     'list_barang': data_barang_wishlist,
-    'nama': 'Kak Cinoy',
-    'last_login': request.COOKIES['last_login'],
+    'nama': 'Febi Claudia Damanik',
 }
     return render(request, "wishlist.html", context)
+
+# lab/tutorial 5
+@login_required(login_url='/wishlist/login/')
+def show_wishlist_ajax(request):
+    data_barang_wishlist = BarangWishlist.objects.all()
+    context = {
+        'list_barang': data_barang_wishlist,
+        'nama': 'Febi Claudia Damanik',
+    }
+    return render(request, 'wishlist_ajax.html', context)
+
+@login_required(login_url='/wishlist/login/')
+def submit_ajax(request):
+    print(request.POST.get('nama_barang'))
+    if request.method == 'POST':
+        nama_barang = request.POST.get('nama_barang')
+        harga_barang = request.POST.get('harga_barang')
+        deskripsi = request.POST.get('deskripsi')
+        new_barang = BarangWishlist(nama_barang=nama_barang, harga_barang=harga_barang, deskripsi=deskripsi)
+        new_barang.save()
+        return HttpResponseRedirect('/wishlist/ajax/')
+    return HttpResponse('post success')
 
 def register(request):
     form = UserCreationForm()
